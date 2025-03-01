0da3c7a262e1a161370fd0247682bb62ed482b7c
huing <huingmc@outlook.com>
2025-03-01 18:47:45 +0800

    代码提交

--- xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
+++ xuecheng-plus-base/src/main/java/com/xuecheng/base/exception/GlobalExceptionHandler.java
@@ -1,8 +1,14 @@
 package com.xuecheng.base.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author huing
@@ -16,8 +22,8 @@ public class GlobalExceptionHandler {
     //@ResponseBody
     @ExceptionHandler(XueChengPlusException.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    public RestErrorResponse customException(XueChengPlusException e){
-        log.error("【系统异常】{}",e.getErrMessage(),e);
+    public RestErrorResponse customException(XueChengPlusException e) {
+        log.error("【系统异常】{}", e.getErrMessage(), e);
         return new RestErrorResponse(e.getErrMessage());
     }
 
@@ -26,10 +32,24 @@ public class GlobalExceptionHandler {
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public RestErrorResponse exception(Exception e) {
 
-        log.error("【系统异常】{}",e.getMessage(),e);
+        log.error("【系统异常】{}", e.getMessage(), e);
 
         return new RestErrorResponse(CommonError.UNKOWN_ERROR.getErrMessage());
 
     }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public RestErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException e) {
+        BindingResult bindingResult = e.getBindingResult();
+        List<String> msgList = new ArrayList<>();
+        //将错误信息放在msgList
+        bindingResult.getFieldErrors().stream().forEach(item -> msgList.add(item.getDefaultMessage()));
+        //拼接错误信息
+        String msg = StringUtils.join(msgList, ",");
+        log.error("【系统异常】{}", msg);
+        return new RestErrorResponse(msg);
+    }
+
 
 }
--- xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
+++ xuecheng-plus-content/xuecheng-plus-content-api/src/main/java/com/xuecheng/content/api/CourseBaseInfoController.java
@@ -1,5 +1,6 @@
 package com.xuecheng.content.api;
 
+import com.xuecheng.base.exception.ValidationGroups;
 import com.xuecheng.base.model.PageParams;
 import com.xuecheng.base.model.PageResult;
 import com.xuecheng.content.model.dto.AddCourseDto;
@@ -10,10 +11,8 @@ import com.xuecheng.content.service.CourseBaseService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -53,11 +52,18 @@ public class CourseBaseInfoController {
 
     @ApiOperation("新增课程基础信息")
     @PostMapping("/course")
-    public CourseBaseInfoDto createCourseBase(@RequestBody AddCourseDto addCourseDto) {
+    public CourseBaseInfoDto createCourseBase(@RequestBody @Validated({ValidationGroups.Inster.class}) AddCourseDto addCourseDto) {
         //机构id，由于认证系统没有上线暂时硬编码
         Long companyId = 1232141425L;
         return courseBaseService.createCourseBase(companyId, addCourseDto);
 
     }
+    @ApiOperation("根据课程id查询课程基础信息")
+    @GetMapping("/course/{courseId}")
+    public CourseBaseInfoDto getCourseBaseById(@PathVariable Long courseId){
+        return null;
+    }
+
+
 
 }
